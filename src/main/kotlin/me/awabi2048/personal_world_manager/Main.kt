0f79ea019c456e80d2389688c099d79687f478a6
awabi2048 <2886xl@gmail.com>
2025-02-10 00:12:12 +0900

    init

---
+++ src/main/kotlin/me/awabi2048/personal_world_manager/Main.kt
@@ -0,0 +1,14 @@
+package me.awabi2048.personal_world_manager
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
