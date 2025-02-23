d3290b4710eb82ece7d382e1e202b0e61fae49af
awabi2048 <2886xl@gmail.com>
2025-02-24 03:04:21 +0900

    ワールドポイントの管理コマンドを実装

--- src/main/kotlin/me/awabi2048/mw_manager/command/PointCommand.kt
+++ src/main/kotlin/me/awabi2048/mw_manager/command/PointCommand.kt
@@ -1,16 +1,73 @@
 package me.awabi2048.mw_manager.command
 
-import me.awabi2048.mw_manager.custom_item.CustomItem
+import me.awabi2048.mw_manager.Main.Companion.prefix
+import me.awabi2048.mw_manager.player_data.PlayerData
+import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabCompleter
-import org.bukkit.entity.Player
 
-object PointCommand: CommandExecutor, TabCompleter {
+// /worldpoint %player% add|subtract|set|get %value%?
+object PointCommand : CommandExecutor, TabCompleter {
     override fun onCommand(p0: CommandSender, p1: Command, p2: String, p3: Array<out String>?): Boolean {
-        if (p0 is Player && p3.isNullOrEmpty()) {
-            p0.inventory.addItem(CustomItem.WORLD_PORTAL.item)
+        if (p3?.size !in 2..3) {
+            p0.sendMessage("$prefix §c無効なコマンドです。")
+            return true
+        }
+
+        // 引数チェック
+        val player = Bukkit.getOfflinePlayer(p3!![0]).player
+        val option = p3[1]
+
+        if (player == null) {
+            p0.sendMessage("$prefix §c該当のプレイヤーが見つかりませんでした。")
+            return true
+        }
+
+        if (option !in listOf("add", "subtract", "set", "get")) {
+            p0.sendMessage("$prefix §c操作が無効です。")
+            return true
+        }
+
+        val data = PlayerData(player)
+
+        // 値の変更を伴う操作
+        if (p3.size == 3) {
+            val value = p3[2].toIntOrNull()
+
+            if (value == null) {
+                p0.sendMessage("$prefix §c入力値が無効です。")
+                return true
+            }
+
+            if (option !in listOf("add", "subtract", "set")) {
+                p0.sendMessage("$prefix §c操作が無効です。")
+                return true
+            }
+
+            // 通知のため値を保管する
+            val modifiedValue = when (option) {
+                "add" -> data.worldPoint + value
+                "subtract" -> data.worldPoint - value
+                "set" -> data.worldPoint
+                else -> data.worldPoint
+            }
+
+            data.worldPoint = modifiedValue
+
+            // 結果の通知
+            p0.sendMessage("$prefix §e${player.displayName}§7のワールドポイントを§a${modifiedValue}§7に変更しました。")
+
+        } else if (p3.size == 2) {
+            if (option != "get") {
+                p0.sendMessage("$prefix §c操作が無効です。")
+                return true
+            }
+
+            // 結果の送信
+            p0.sendMessage("$prefix §e${player.displayName}§7のワールドポイントは、現在§a${data.worldPoint}です。")
+
         }
 
         return true
--- src/main/kotlin/me/awabi2048/mw_manager/player_data/PlayerData.kt
+++ src/main/kotlin/me/awabi2048/mw_manager/player_data/PlayerData.kt
@@ -1,32 +1,15 @@
 package me.awabi2048.mw_manager.player_data
 
 import me.awabi2048.mw_manager.config.DataFiles
+import me.awabi2048.mw_manager.my_world.MyWorld
+import org.bukkit.configuration.Configuration
+import org.bukkit.configuration.ConfigurationSection
 import org.bukkit.entity.Player
 
 data class PlayerData(val player: Player) {
     private val uuid = player.uniqueId.toString()
 
-    val worldPoint: Int
-        get() {
-            return DataFiles.playerData.getInt("$uuid.world_point")
-        }
-
-    val unlockedWarpSlot: Int
-        get() {
-            return DataFiles.playerData.getInt("$uuid.unlocked_warp_slot")
-        }
-
-    val unlockedWorldSlot: Int
-        get() {
-            return DataFiles.playerData.getInt("$uuid.unlocked_world_slot")
-        }
-
-    val warpShortcuts: List<String>
-        get() {
-            return DataFiles.playerData.getStringList("$uuid.warp_shortcuts")
-        }
-
-    fun initialize() {
+    private fun initialize() {
         val dataSection = mapOf(
             "world_point" to 0,
             "unlocked_warp_slot" to DataFiles.config.getInt("default_unlocked_warp_slot"),
@@ -36,4 +19,55 @@ data class PlayerData(val player: Player) {
         DataFiles.playerData.createSection(uuid, dataSection)
         DataFiles.save()
     }
+
+    init {
+        if (DataFiles.playerData.contains(uuid)) initialize()
+    }
+
+    private val section: ConfigurationSection
+        get() {
+            return DataFiles.playerData.getConfigurationSection(uuid)!!
+        }
+
+    var worldPoint: Int
+        get() {
+            return section.getInt("world_point")
+        }
+
+        set(value) {
+            DataFiles.playerData.set("$uuid.world_point", value.coerceAtLeast(0))
+            DataFiles.save()
+        }
+
+    var unlockedWarpSlot: Int
+        get() {
+            return section.getInt("unlocked_warp_slot")
+        }
+
+        set(value) {
+            DataFiles.playerData.set("$uuid.unlocked_warp_slot", value.coerceAtLeast(0))
+            DataFiles.save()
+        }
+
+    var unlockedWorldSlot: Int
+        get() {
+            return section.getInt("unlocked_world_slot")
+        }
+
+        set(value) {
+            DataFiles.playerData.set("$uuid.unlocked_world_slot", value.coerceAtLeast(0))
+            DataFiles.save()
+        }
+
+    var warpShortcuts: List<String>
+        get() {
+            return section.getStringList("warp_shortcuts")
+        }
+
+        set(value) {
+            val uuidList = value.filter { MyWorld(it).isRegistered }
+
+            DataFiles.playerData.set("$uuid.warp_shortcuts", uuidList)
+            DataFiles.save()
+        }
 }
